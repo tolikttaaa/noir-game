0dc9a512dea154fa0d0b313987fc15f4d0a3c6cc
a.anishchenko <a.anishchenko@tinkoff.ru>
2022-11-08 16:44:52 +0400

    Initial commit

---
+++ src/main/kotlin/com/ttaaa/noirgame/NoirGameApplication.kt
@@ -0,0 +1,11 @@
+package com.ttaaa.noirgame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NoirGameApplication
+
+fun main(args: Array<String>) {
+    runApplication<NoirGameApplication>(*args)
+}
---
+++ src/test/kotlin/com/ttaaa/noirgame/NoirGameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ttaaa.noirgame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NoirGameApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
